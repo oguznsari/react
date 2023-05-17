b48dde7467d71cb97301121d921bdd4ff3fe07fd
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-17 17:51:46 +0300

    move data & fetch it & delete functionality for appointment button

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -1,10 +1,23 @@
+import { useState, useEffect, useCallback } from "react";
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
-import appointmentList from "./data.json";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      })
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
@@ -14,7 +27,11 @@ function App() {
 
       <ul className="divide-y divide-gray-200">
         {appointmentList.map(appointment => (
-          <AppointmentInfo key={appointment.id} appointment={appointment} />
+          <AppointmentInfo key={appointment.id} appointment={appointment} onDeleteAppointment={
+            appointmentId =>
+              setAppointmentList(appointmentList.filter(appointment =>
+                appointment.id != appointmentId))
+          } />
         ))
         }
       </ul>
