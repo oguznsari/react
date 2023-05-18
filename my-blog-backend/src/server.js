a72b5092d1c3a1fdacd469f60d97ab035f4f70a7
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-18 15:49:11 +0300

    upvote api endpoint + nodemon

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 
+let articlesInfo = [
+    {
+        name: "learn-react",
+        upvotes: 0
+    },
+    {
+        name: "learn-node",
+        upvotes: 0
+    },
+    {
+        name: "mongodb",
+        upvotes: 0
+    }
+];
+
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    console.log(req.body);
-    res.send(`Hello! ${req.body.name}`);
-});
-
-app.get('/hello/:name', (req, res) => {
+app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
-    res.send(`Hello ${name}`);
+    const artilce = articlesInfo.find(a => a.name === name);
+
+    if (artilce) {
+        artilce.upvotes += 1;
+        res.send(`The ${name} article now has ${artilce.upvotes} upvotes!!!`)
+    } else {
+        res.send('That article doesn\'t exists.');
+    }
 });
 
 app.listen(8000, () => {
