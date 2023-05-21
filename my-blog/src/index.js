6b4b6e26c5205b98ac7a49f1c7f84a1e1ebbdc59
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-21 20:37:32 +0300

    firebase + login page & route + create account page & route

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBllft5YJWVvnlR8b-njX7ejUXLRUlhEYs",
+  authDomain: "my-react-blog-c0cd9.firebaseapp.com",
+  projectId: "my-react-blog-c0cd9",
+  storageBucket: "my-react-blog-c0cd9.appspot.com",
+  messagingSenderId: "139223446052",
+  appId: "1:139223446052:web:4ccf7cb9d2bff07b1d1eb1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
