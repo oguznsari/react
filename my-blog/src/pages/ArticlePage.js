6e6bbd9f3e21202ebbdabf00913dd8cf1dc65138
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-21 18:56:23 +0300

    axios comments List & show upvotes (axios call)

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,10 +1,26 @@
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CommentsList from "../components/CommentsList";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`)
+            const articleInfo = response.data.article;
+            setArticleInfo(articleInfo);
+        }
+
+        loadArticleInfo()
+    }, [])
+
 
     if (!article) {
         return <NotFoundPage />
@@ -13,9 +29,11 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This Article has: {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, i) => (
                 <p key={i}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
         </>
     )
 }
