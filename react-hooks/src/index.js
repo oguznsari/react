fdddf4f45f97bb9103cbcd241fb6b7c2004f93f7
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 13:21:17 +0300

    re-use with custom hooks

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import useInput from './useInput';
 
 function App() {
-  const [sound, setSound] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    alert(`${sound} sounds like ${color}.`);
-    setSound("");
-    setColor("#000000");
+    alert(`${titleProps.value} sounds like ${colorProps.value}.`);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={submit}>
-      <input value={sound} type='text' placeholder='Sound...'
-        onChange={(e) => setSound(e.target.value)}
+      <input {...titleProps} type='text' placeholder='Sound...'
       />
-      <input value={color} type='color'
-        onChange={(e) => setColor(e.target.value)}
+      <input {...colorProps} type='color'
       />
       <button>Add</button>
     </form>
---
+++ react-hooks/src/useInput.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(
+        initialValue
+    );
+
+    return [
+        {
+            value,
+            onChange: (e) => setValue(e.target.value)
+        },
+        () => setValue(initialValue)
+    ]
+}
+
+export default useInput
