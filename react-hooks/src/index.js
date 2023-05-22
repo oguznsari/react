f4ca790734b458b726013f60e260f52e5f129971
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 11:29:00 +0300

    fetch data with useEffect, check dependency array etc

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -3,24 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App() {
-  const [name, setName] = useState("Jan");
-  const [admin, setAdmin] = useState(false);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    document.title = `Celebrate ${name}`
-  }, [name]);
+    fetch(`https://api.github.com/users`)
+      .then(response => response.json())
+      .then(res => setData(res))
+  }, []);
 
-  useEffect(() => {
-    console.log(`The user is: ${admin ? "admin" : "not admin"}.`)
-  }, [admin]);
+  if (data) {
+    return (
+      <div>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+        <button onClick={() => setData([])}>Remove Data</button>
+      </div>
+    )
+  }
 
   return (
-    <section>
-      <p>Congratulations {name}!</p>
-      <button onClick={() => setName("Will")}>Change Winner</button>
-      <p>{admin ? "logged in" : "not logged in"}</p>
-      <button onClick={() => setAdmin(true)}>Log In</button>
-    </section>
+    <p>No users.</p>
   )
 }
 
