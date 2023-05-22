72a40b70667996ead089125abb26c4e90abec072
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 10:59:16 +0300

    Start from react icons || sending interactions up to component trees

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [
+  ...Array(length)
+];
+
+function Star({ selected = false, onSelect }) {
+  return (
+    <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />
+  )
+}
+
+function StarRating({ totalStarts = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStarts).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => { setSelectedStars(i + 1) }}
+        />
+      ))}
+      <p>{selectedStars} of {totalStarts}</p>
+    </>
+  );
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
   return (
-    <div>
-      <input
-        type='checkbox'
-        value={checked}
-        onChange={() => {
-          setChecked((checked) => !checked)
-        }}
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
+    <StarRating totalStarts={4} />
   )
 }
 
