870397407af454f3fff07a05c3a203de88822ba5
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 11:19:25 +0300

    useEffect with dependency array

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,37 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FaStar } from "react-icons/fa";
-
-const createArray = (length) => [
-  ...Array(length)
-];
-
-function Star({ selected = false, onSelect }) {
-  return (
-    <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />
-  )
-}
-
-function StarRating({ totalStarts = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStarts).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => { setSelectedStars(i + 1) }}
-        />
-      ))}
-      <p>{selectedStars} of {totalStarts}</p>
-    </>
-  );
-}
 
 function App() {
+  const [name, setName] = useState("Jan");
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    document.title = `Celebrate ${name}`
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`The user is: ${admin ? "admin" : "not admin"}.`)
+  }, [admin]);
+
   return (
-    <StarRating totalStarts={4} />
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName("Will")}>Change Winner</button>
+      <p>{admin ? "logged in" : "not logged in"}</p>
+      <button onClick={() => setAdmin(true)}>Log In</button>
+    </section>
   )
 }
 
