8ae8446705bcf770d47003b876641b4ac38ca588
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 10:08:55 +0300

    useState()

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App({ name }) {
+function App() {
   return (
     <div className="App">
-      <h1>Hello {name}!</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const [, , third] = ["Alex", "Ali", "Anna"];
-console.log({ third });
+function App() {
+  const [status, setStatus] = useState("Not delivered");
+  return (
+    <div>
+      <h1>The package is: {status} </h1>
+      <button
+        onClick={() => setStatus("Delivered")}
+      >
+        Deliver
+      </button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="Oğuzhan" />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
