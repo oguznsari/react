1aeb838756033f383aabe7f3250a49dab206a9f1
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-22 13:54:20 +0300

    useContext -> wrap and use

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { TreesContext } from '.';
+import { useContext } from 'react';
 
 function App() {
+  const { trees } = useContext(TreesContext);
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1>
+        Trees I've heard of
+      </h1>
+      <ul>
+        {trees.map((tree) => {
+          return (
+            <li key={tree.id}>{tree.type}</li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import useInput from './useInput';
+import App from './App';
 
-function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
+export const TreesContext = createContext();
 
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value} sounds like ${colorProps.value}.`);
-    resetTitle();
-    resetColor();
-  };
-
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type='text' placeholder='Sound...'
-      />
-      <input {...colorProps} type='color'
-      />
-      <button>Add</button>
-    </form>
-  )
-}
+const trees = [
+  { id: "1", type: "Maple" },
+  { id: "2", type: "Oak" },
+  { id: "3", type: "Family" },
+  { id: "4", type: "Component" }
+]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <TreesContext.Provider value={{ trees }}>
     <App />
-  </React.StrictMode>
+  </TreesContext.Provider>
 );
