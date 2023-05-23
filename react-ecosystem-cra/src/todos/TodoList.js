f992c6e1b15d5b5e6f579bf656159e1f1d9ba9bb
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-23 15:06:25 +0300

    run react-redux app

--- react-ecosystem-cra/src/todos/TodoList.js
+++ react-ecosystem-cra/src/todos/TodoList.js
@@ -2,14 +2,29 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css"
 import NewTodoForm from "./NewTodoForm";
+import { connect } from "react-redux";
+import { removeTodo } from "./actions";
 
-const TodoList = ({ todos = [] }) => {
+const TodoList = ({ todos = [], onRemovePressed }) => {
     return (
         <div className="list-wrapper">
             <NewTodoForm />
-            {todos.map(todo => <TodoListItem todo={todo} />)}
+            {todos.map(todo =>
+                <TodoListItem
+                    todo={todo}
+                    onRemovePressed={onRemovePressed}
+                />
+            )}
         </div>
     )
 }
 
-export default TodoList;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
