e3490d9f7eb12ba225cf7b8e754ff705b4a4d239
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-23 17:28:18 +0300

    adding redux thunk to react

--- react-ecosystem-cra/src/store.js
+++ react-ecosystem-cra/src/store.js
@@ -1,8 +1,10 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 import { todos } from "./todos/reducers";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = {
     todos,
@@ -20,6 +22,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const configureStore = () =>
     createStore(
         persistedReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
+        composeWithDevTools(
+            applyMiddleware(thunk)
+        )
     ); 
