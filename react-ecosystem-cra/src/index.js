4c50903dc4caa19b22948a91b9255b9937398e87
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-23 15:57:46 +0300

    Persisting the redux store

--- react-ecosystem-cra/src/index.js
+++ react-ecosystem-cra/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate
+      persistor={persistor}
+      loading={<div>Loading...</div>}
+    >
+      <App />
+    </PersistGate>
   </Provider>
 );
 
