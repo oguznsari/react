c505ece1257df54078447f5eef3b0230208ddbc6
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-24 23:00:24 +0300

    verify JWT

--- react-authentication/back-end/src/routes/updataUserInfoRoute.js
+++ react-authentication/back-end/src/routes/updataUserInfoRoute.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { ObjectId } from "mongoose";
 import { initializeDbConnection } from "../db";
 import { StatusCodes } from "http-status-codes";
+import User from "../models/User";
 
 export const updateUserInfoRoute = {
     path: "/api/users/:userId",
@@ -27,5 +28,35 @@ export const updateUserInfoRoute = {
         }
 
         const token = authorization.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res
+                .status(StatusCodes.UNAUTHORIZED)
+                .json({ message: "Unable to verify token." })
+
+            const { id } = decoded;
+            if (id !== userId) return res
+                .status(StatusCodes.FORBIDDEN)
+                .json({ message: "Not allowed to update that user's data." })
+
+            await initializeDbConnection(process.env.MONGO_URI);
+            const result = await User.findOneAndUpdate(
+                { _id: ObjectId(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false }
+            );
+
+            const { email, isVerified, info } = result.value;
+            jwt.sign(
+                { id, email, isVerified, info },
+                process.env.JWT_SECRET,
+                { expiresIn: process.env.JWT_LIFETIME },
+                (err, token) => {
+                    if (err) return res.status(StatusCodes.OK).json({ err });
+                    return res.status(StatusCodes.OK).json({ token });
+                }
+            );
+        });
+
+
     }
 }
