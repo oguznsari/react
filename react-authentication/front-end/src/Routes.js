9a7294546c66fdd5fab1db4b34486e19a06756a1
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-24 16:17:51 +0300

    auto redirect to login page for PrivateRoutes(created)

--- react-authentication/front-end/src/Routes.js
+++ react-authentication/front-end/src/Routes.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserInfoPage } from './pages/UserInfoPage';
 import { LogInPage } from './pages/LogInPage';
 import { SignUpPage } from './pages/SignUpPage';
+import { PrivateRoute } from './auth/PrivateRoute';
 
 export const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact><UserInfoPage /></Route>
+                <PrivateRoute path="/" exact><UserInfoPage /></PrivateRoute>
                 <Route path="/login"><LogInPage /></Route>
                 <Route path="/signup"><SignUpPage /></Route>
             </Switch>
