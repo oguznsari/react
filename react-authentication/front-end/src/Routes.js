3c1c358a1e9825747a70f10fddf800629d394ff6
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-25 18:36:26 +0300

    Password Reset Landing page & success and fail pages & routing

--- react-authentication/front-end/src/Routes.js
+++ react-authentication/front-end/src/Routes.js
@@ -6,6 +6,7 @@ import { PrivateRoute } from './auth/PrivateRoute';
 import { PleaseVerifyEmailPage } from './pages/PleaseVerifyEmailPage';
 import { EmailVerificationLandingPage } from './pages/EmailVerificationLandingPage';
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
+import { PasswordResetLandingPage } from './pages/PasswordResetLanding';
 
 export const Routes = () => {
     return (
@@ -17,6 +18,7 @@ export const Routes = () => {
                 <Route path="/please-verify"><PleaseVerifyEmailPage /></Route>
                 <Route path="/verify-email/:verificationString"><EmailVerificationLandingPage /></Route>
                 <Route path="/forgot-password"><ForgotPasswordPage /></Route>
+                <Route path="/reset-password/:passwordResetCode"><PasswordResetLandingPage /></Route>
             </Switch>
         </Router>
     );
---
+++ react-authentication/front-end/src/pages/PasswordResetSuccess.js
@@ -0,0 +1,17 @@
+import { useHistory } from "react-router-dom";
+
+export const PasswordResetSuccess = () => {
+    const history = useHistory();
+
+    return (
+        <div className="content-container">
+            <h1>Success!</h1>
+            <p>
+                Your Password has been reset, now please login with your new password.
+            </p>
+            <button
+                onClick={() => history.push('/login')}
+            >Go to login page</button>
+        </div>
+    )
+}
