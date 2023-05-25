18c7f1eb13acaa9e9b55feb4f421a706e6601daa
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-25 23:16:25 +0300

    adding Oauth callback route

--- react-authentication/back-end/src/routes/index.js
+++ react-authentication/back-end/src/routes/index.js
@@ -7,6 +7,7 @@ import { testEmailRoute } from './testEmailRoute';
 import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmail';
+import { googleOauthCallbackRoute } from './googleOauthCallbackRoute';
 
 export const routes = [
     testRoute,
@@ -17,5 +18,6 @@ export const routes = [
     verifyEmailRoute,
     forgotPasswordRoute,
     resetPasswordRoute,
-    getGoogleOauthUrlRoute
+    getGoogleOauthUrlRoute,
+    googleOauthCallbackRoute
 ];
