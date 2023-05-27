9591d63b6556704ce2fde6dcdfc96f013d785ec1
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-27 16:34:50 +0300

    .next out & initial components from template

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -1,6 +1,19 @@
 import styles from "./index.module.css";
 import Card from './Card';
 
-export default () => (
-    <p>Hello</p>
-);
+export default () => {
+    return (
+        <div className={styles.app}>
+            <header className={styles.header}>
+                <img src="/logo.png"
+                    className={styles.logo} alt="logo"
+                />
+            </header>
+            <div className={styles.grid}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
+};
