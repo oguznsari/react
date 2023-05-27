eafddd5687e00ac258959c2e8b8d2f1848826e3d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-05-27 17:03:40 +0300

    add data for props

--- ssr/pages/index.js
+++ ssr/pages/index.js
@@ -1,19 +1,30 @@
+import React from "react";
 import styles from "./index.module.css";
 import Card from './Card';
+import data from "../API/data.json";
 
-export default () => {
-    return (
-        <div className={styles.app}>
-            <header className={styles.header}>
-                <img src="/logo.png"
-                    className={styles.logo} alt="logo"
-                />
-            </header>
-            <div className={styles.grid}>
-                <Card />
-                <Card />
-                <Card />
+
+export default class Index extends React.Component {
+    static async getInitialProps() {
+        return { cards: data };
+    }
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <header className={styles.header}>
+                    <img src="/logo.png"
+                        className={styles.logo} alt="logo"
+                    />
+                </header>
+                <div className={styles.grid}>
+                    {
+                        this.props.cards.map((card) => (
+                            <Card key={card.id} />
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
